1604cd378f05144616e3b1833f94742ca763aa75
Lucas Boarini <lcsboarinireis92@gmail.com>
2023-04-02 19:28:26 -0300

    :lipstick: Design parte desktop finalizada, faltam pop-up

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -1,12 +1,23 @@
+/*   --Tag's HTML--   */
 const btnMenuBurguer = document.querySelector('#btn-burguer-img');
 const btnCloseMenu = document.querySelector('#btn-close-menu');
+const principalImage = document.querySelector('.principal-image');
 
+/*   --Functions--   */
 
-/* dunção para menu hamburguer */
+/* Function Toggle Menu */
 function toggleMenu() {
     const menuBurguer = document.querySelector('#menu-burguer');
 
     menuBurguer.classList.toggle('active');
+}
+
+function changeImage(image) {
+    if (image == 1) {
+        principalImage.style.background = "url(../images/image-product-1.jpg)";
+    } else if (image == 2) {
+        principalImage.style.background = "url(../images/image-product-2.jpg);";
+    }
 }
 
 
